7e3a5255a779ae2ef84e63e28551c1620b44b68e
Wanderson <you@example.com>
2023-04-26 10:24:20 -0300

    Estrutura estática

---
+++ PomodoroTimer/src/Header.jsx
@@ -0,0 +1,32 @@
+//import { useState } from 'react'
+
+
+function Header() {
+
+  return (
+    <>
+      <div>
+        <ul>
+            <li>PomoFocus</li>
+            <li>
+                <button>
+                    Relatório
+                </button>
+            </li>
+            <li>
+                <button>
+                    Configurações
+                </button>
+            </li>
+            <li>
+                <button>
+                    Logo
+                </button>
+            </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default Header
