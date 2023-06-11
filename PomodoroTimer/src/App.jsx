39c42bc568a327961ae8458789f16dbed75f29fb
Nascimento W <wandersonnascimento94@gmail.com>
2023-06-11 17:21:43 -0300

    os diferentes estados e parando no zero

--- PomodoroTimer/src/App.jsx
+++ PomodoroTimer/src/App.jsx
@@ -1,40 +1,76 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from "./Header"
 import Timer from "./Timer"
 import './App.css'
 
 function App() {
   const [isPausado, setIsPausado] = useState(false);
+  const [isPomodoro, setIsPomodoro] = useState(false);
+  const [isPausaCurta, setIsPausaCurta] = useState(false);
+  const [isPausaLonga, setIsPausaLonga] = useState(false);
+
 
   function handleClick() {
     setIsPausado(prevIsPausado => !prevIsPausado);
   }
 
+  function handlePomodoro(){
+    setIsPomodoro(true);
+    setIsPausado(true);
+    setIsPausaCurta(false);
+    setIsPausaLonga(false);
+  }
+
+  function handlePausaCurta(){
+    setIsPausaCurta(true);
+    setIsPausado(true);
+    setIsPomodoro(false);
+    setIsPausaLonga(false);
+  }
+
+  function handlePausalonga(){
+    setIsPausaLonga(true);
+    setIsPausado(true);
+    setIsPausaCurta(false);
+    setIsPomodoro(false);
+  }
+
+
+  // 
+    let backgroundColor = 'rgb(186, 73, 73)'; // Cor vermelho padrão
+
+    if (isPausaCurta) {
+      backgroundColor = 'rgb(56, 133, 138)'; // Cor azul claro para pausa curta
+    } else if (isPausaLonga) {
+      backgroundColor = 'rgb(57, 112, 151)'; // Cor azul escuro para pausa longa
+    }
+
   return (
     <>
+    <div style={{backgroundColor}}>
       <Header />
-      <div className="central">
+      <div className="central" >
         <div className="options">
           <ul>
             <li>
-              <button>
-                Pomodoro
+              <button onClick={handlePomodoro}>
+                Pomodoro 
               </button>
             </li>
             <li>
-              <button>
-                Pausa curta
+              <button onClick={handlePausaCurta}>
+                Pausa curta 
               </button>
             </li>
             <li>
-              <button>
+              <button onClick={handlePausalonga}>
                 Pausa longa
               </button>
             </li>
           </ul>
         </div>
         <div className="timer">
-          <Timer isPausado={isPausado} />
+          <Timer isPausado={isPausado} isPomodoro={isPomodoro} isPausaCurta={isPausaCurta} isPausaLonga={isPausaLonga} handleClick={handleClick}/>
         </div>
         <div className="functions">
           {isPausado ? <button onClick={handleClick}>COMEÇAR</button> : <button onClick={handleClick}>PAUSAR</button>}
@@ -42,6 +78,7 @@ function App() {
         </div>
       </div>
       <div>
+      </div>
       </div>
     </>
   )
--- PomodoroTimer/src/Timer.jsx
+++ PomodoroTimer/src/Timer.jsx
@@ -2,23 +2,46 @@ import { useEffect } from 'react';
 import { useState } from 'react'
 import './App.css'
 
-function Timer(props) {
+function Timer({isPausado, isPomodoro, isPausaCurta, isPausaLonga, handleClick}) {
     const [segundos, setSegundos] = useState(1500);
-    const [minutos, setMinutos] = useState(0);
+
+    useEffect(() => {
+        if(isPausaCurta){
+            setSegundos(300);
+        }
+    },[isPausaCurta]);
+
+    useEffect(() => {
+        if(isPausaLonga){
+            setSegundos(900);
+        }
+    },[isPausaLonga]);
+
+    useEffect(() => {
+        if(isPomodoro){
+            setSegundos(1500);
+        }
+    },[isPomodoro]);
+
+    useEffect(() => {
+        if(segundos <= 0){
+            handleClick();
+        }
+    },[segundos])
 
 
     useEffect(() => {
         const interval = setInterval(() => {
-            if (!props.isPausado) {
+            if (!isPausado && segundos>0) {
                 setSegundos(prevSegundos => prevSegundos - 1)
             }
-        }, 1000);
+        }, 1);
 
         return () => {
             clearInterval(interval);
         };
 
-    }, [props.isPausado])
+    }, [isPausado])
 
     const minutosDisplay = String(Math.floor(segundos / 60)).padStart(2, '0');
     const segundosDisplay = String(Math.ceil(segundos % 60)).padStart(2, '0');
