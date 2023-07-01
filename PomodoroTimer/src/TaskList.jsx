e011db62e7023f89d092457ce943515e51cca996
Wanderson <you@example.com>
2023-07-01 14:26:36 -0300

    Lista de tarefas inicial

--- PomodoroTimer/src/TaskList.jsx
+++ PomodoroTimer/src/TaskList.jsx
@@ -1,20 +1,38 @@
-import { useEffect, useState} from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function TaskList({ isPausado, isPomodoro, isPausaCurta, isPausaLonga, handleClick, handlePausaCurta }) {
-    const [estado, setEstado] = useState(0); 
+function TaskList({
+  isPausado,
+  isPomodoro,
+  isPausaCurta,
+  isPausaLonga,
+  handleClick,
+  handlePausaCurta,
+}) {
+  const [estado, setEstado] = useState(0);
+  const people = [
+    "Fazer compras",
+    "Estudar JavaScript",
+    "Lavar o carro",
+    "Preparar o jantar",
+    "Fazer exercícios",
+  ];
+  const listItems = people.map((person) => (
+    <li className="taskListElements" key={person}>
+      {person} <input type="checkbox" />{" "}
+    </li>
+  ));
 
-
-    return (
-        <>
-            <div className="taskList">
-                <div>
-                   Lista de tarefa
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="footer">
+        <div>
+          Lista de tarefa
+          <ul className="taskListGeral">{listItems}</ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default TaskList
-
+export default TaskList;
